507d6d7b8a7a8bfb1c10db49a82491ecef376b22
Gaurav Mehra <gaurav9759434559@gmail.com>
2025-06-27 17:43:07 +0530

    first

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-if(process.env.NODE_ENV != "production"){
-require('dotenv').config()
-}
+// if(process.env.NODE_ENV != "production"){
+// require('dotenv').config()
+// }
 
 
 
-
+require('dotenv').config();
 
 
 const express = require("express");
